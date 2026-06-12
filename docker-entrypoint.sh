e135550bfa916d19a666845139ab7f4d466a328a
Vignesh <ks2vignesh@gmail.com>
2026-06-13 04:02:31 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 set -e
 
-echo "=== DEBUG START ==="
+mkdir -p /workspace/models
 
-which hf || true
+if [ ! -d "/workspace/models/SoulX-FlashHead-1_3B" ]; then
+    echo "Downloading SoulX model..."
+    hf download \
+        Soul-AILab/SoulX-FlashHead-1_3B \
+        --local-dir /workspace/models/SoulX-FlashHead-1_3B
+fi
 
-hf --help || true
+if [ ! -d "/workspace/models/wav2vec2-base-960h" ]; then
+    echo "Downloading wav2vec model..."
+    hf download \
+        facebook/wav2vec2-base-960h \
+        --local-dir /workspace/models/wav2vec2-base-960h
+fi
 
-pip show huggingface_hub || true
+echo "Starting FastAPI..."
 
-echo "=== DEBUG END ==="
-
-sleep infinity
+uvicorn dual-mode-worker.fastapi_server:app \
+    --host 0.0.0.0 \
+    --port 8000
