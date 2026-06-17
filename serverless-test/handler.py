84ff9a4473e5d23c2b2773e3bb2cd19f09f0befe
Vignesh <ks2vignesh@gmail.com>
2026-06-18 00:12:13 +0530

    Update handler.py

--- serverless-test/handler.py
+++ serverless-test/handler.py
@@ -1,16 +1,17 @@
 print("START")
 
+with open("/tmp/test.txt", "w") as f:
+    f.write("HELLO")
+
 import torch
 print("TORCH IMPORTED")
 
-def handler(job):
-    print("HANDLER")
-    print(job)
-
-    return {
-        "received": job
-    }
-
 import runpod
+print("RUNPOD IMPORTED")
 
+def handler(job):
+    print("HANDLER CALLED")
+    return {"ok": True}
+
+print("REGISTERING")
 runpod.serverless.start({"handler": handler})
