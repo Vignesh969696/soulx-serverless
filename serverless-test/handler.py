50af431a02289177b9b26001c6732e5e8625ef56
Vignesh <ks2vignesh@gmail.com>
2026-06-17 22:41:00 +0530

    Update handler.py

--- serverless-test/handler.py
+++ serverless-test/handler.py
@@ -1,1 +1,13 @@
+print("STARTING FILE")
 
+import runpod
+
+print("IMPORTED RUNPOD")
+
+def handler(job):
+    print("HANDLER CALLED")
+    return {"message": "hello"}
+
+print("REGISTERING")
+
+runpod.serverless.start({"handler": handler})
