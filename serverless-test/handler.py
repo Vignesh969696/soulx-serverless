1d655d1aa420a337ed1646310c47178972ea594c
Vignesh <ks2vignesh@gmail.com>
2026-06-19 12:40:36 +0530

    Update handler.py

--- serverless-test/handler.py
+++ serverless-test/handler.py
@@ -1,22 +1,22 @@
-print("START")
+print("START", flush=True)
 
 import time
 START_TIME = time.time()
 
 import runpod
-print("RUNPOD IMPORTED")
+print("RUNPOD IMPORTED", flush=True)
 
 
 def handler(job):
     uptime = time.time() - START_TIME
 
-    print("HANDLER CALLED")
-    print("UPTIME", uptime)
+    print("HANDLER CALLED", flush=True)
+    print("UPTIME", uptime, flush=True)
 
     return {
         "uptime": uptime
     }
 
 
-print("REGISTERING")
+print("REGISTERING", flush=True)
 runpod.serverless.start({"handler": handler})
