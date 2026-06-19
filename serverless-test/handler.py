20f342147827d36a91530c6b4a8e793a0c9877c0
Vignesh <ks2vignesh@gmail.com>
2026-06-19 11:30:45 +0530

    Update handler.py

--- serverless-test/handler.py
+++ serverless-test/handler.py
@@ -1,14 +1,20 @@
 print("START")
 
+import time
+START_TIME = time.time()
+
 import runpod
 print("RUNPOD IMPORTED")
 
 
 def handler(job):
+    uptime = time.time() - START_TIME
+
     print("HANDLER CALLED")
+    print("UPTIME", uptime)
 
     return {
-        "version": 999
+        "uptime": uptime
     }
 
 
